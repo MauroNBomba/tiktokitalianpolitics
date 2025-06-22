14815f0a9cf248a5b6a05ddaba6469fe3d489f3d
MauroNBomba <mauro.bomba@uniroma1.it>
2025-06-22 20:06:03 +0200

    Add files via upload

--- esperimento_tiktok.py
+++ esperimento_tiktok.py
@@ -38,8 +38,9 @@ if id_partecipante:
         st.subheader("Interfaccia Admin - Download CSV")
         for file in output_folder.glob("*.csv"):
             st.download_button(label=f"Scarica {file.name}", data=file.read_bytes(), file_name=file.name)
+
     elif id_partecipante in assegnazioni["PartecipantID"].values:
-    user_data = assegnazioni[assegnazioni["PartecipantID"] == id_partecipante].iloc[0]
+        user_data = assegnazioni[assegnazioni["PartecipantID"] == id_partecipante].iloc[0]
         valutazioni = {}
         for i in range(1, 16):
             video_id = user_data[f"video{i}"]
@@ -57,5 +58,6 @@ if id_partecipante:
                 # Salva anche su Google Sheet
                 worksheet.append_row([id_partecipante] + list(valutazioni.values()))
                 st.success("Risposte inviate con successo.")
+
     else:
         st.warning("ID partecipante non valido.")
