1e710044f34f1e573ab5e96a32fc0bf86caf8325
MauroNBomba <mauro.bomba@uniroma1.it>
2025-06-23 13:00:38 +0200

    Add files via upload

--- esperimento_tiktok.py
+++ esperimento_tiktok.py
@@ -56,8 +56,8 @@ if participant_id:
                 Stiamo lavorando ad un progetto che vuole studiare come i meccanismi di fiducia e sfiducia nella politica
                 si legano ad alcune caratteristiche dei messaggi politici.
 
-                Vedrai 15 video tra quelli che i politici italiani hanno postato su TikTok. Ti chiediamo di giudicarli in base a quattro aggettivi,
-                per ciascuno di questi puoi esprimere un giudizio da 1 a 5 in base a quanto lo ritieni adatto a descrivere il video.
+                Vedrai 15 video tra quelli che i politici italiani hanno postato su TikTok durante la campagna elettorale per le Elezioni Europee del 2024.
+                Ti chiediamo di giudicarli in base a quattro aggettivi, per ciascuno di questi puoi esprimere un giudizio da 1 a 5 in base a quanto lo ritieni adatto a descrivere il video.
 
                 Sappiamo che è difficile, ma ti chiediamo di mettere da parte (solo per questa volta) il tuo giudizio politico, le tue idee, e le tue simpatie.
                 Giudica esclusivamente il contenuto del video. È molto importante per noi.
@@ -65,7 +65,7 @@ if participant_id:
                 Sappiamo anche che è un lavoro ripetitivo e noioso, ma sappiamo anche che lo farai al meglio.
                 Per questo ti ringraziamo del tuo tempo. È molto importante per noi.
                 """)
-                if st.button("👉 Inizia il test"):
+                if st.button("Inizia il test"):
                     st.session_state.intro_shown = True
                 st.stop()
 
@@ -83,7 +83,7 @@ if participant_id:
                 aff = st.slider(f"Affidabilità (Video {i + 1})", 1, 5, 1, key=f"aff_{i}")
                 conc = st.slider(f"Concretezza (Video {i + 1})", 1, 5, 1, key=f"conc_{i}")
                 comp = st.slider(f"Competenza (Video {i + 1})", 1, 5, 1, key=f"comp_{i}")
-                if st.button("➡️ Avanti"):
+                if st.button("Avanti"):
                     st.session_state.responses.append({
                         "participantID": participant_id,
                         "videoID": row["videoID"],
@@ -94,7 +94,7 @@ if participant_id:
                         "Competenza": comp
                     })
                     st.session_state.video_index += 1
-                    st.experimental_rerun()
+                    st.rerun()
             else:
                 st.markdown("## ✅ Hai completato tutte le valutazioni.")
                 if st.button("📤 Invia le risposte"):
